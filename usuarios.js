5f94b6437eac711b0a7e02fb37b4e1387189bb78
asiscompras2-cmd <asiscompras2@eltesoro.com.co>
2026-08-04 16:21:43 -0500

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,30 @@
+const usuarios = [
+    {
+        cedula: "1036613213",
+        nombre: "AGUIRRE BOTERO YENNY CATALINA"
+    },
+    {
+        cedula: "71786490",
+        nombre: "DE AVILA FRANCO GUSTAVO ENRIQUE"
+    },
+    {
+        cedula: "43870478",
+        nombre: "GONZALEZ OSPINA ANDREA"
+    },
+    {
+        cedula: "43875008",
+        nombre: "LONDOÑO SEPULVEDA ELIZABETH"
+    },
+    {
+        cedula: "15385664",
+        nombre: "MARTINEZ BEDOYA CONRADO EFREN"
+    },
+    {
+        cedula: "42138570",
+        nombre: "RENGIFO ALVAREZ HELENA"
+    },
+    {
+        cedula: "1152455096",
+        nombre: "VALLEJO TORRES ALEJANDRO"
+    }
+];
